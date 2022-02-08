0e5408ca5b3c0051fdc34a0314a2f666360b03c0
djayamohan <djayamoh@chicagobooth.edu>
2022-02-08 23:43:34 +0000

    finished but errors

--- app/controllers/math_controller.rb
+++ app/controllers/math_controller.rb
@@ -12,4 +12,41 @@ class MathController < ApplicationController
     render({ :template => "math_operations/subtract_results.html.erb" })
   end
 
+  def adder
+    render({ :template => "math_operations/add_form.html.erb" })
+  end
+
+  def adder_results
+    @first = params.fetch("first_input").to_f
+    @second = params.fetch("second_input").to_f
+
+    @result = @second + @first
+    render({ :template => "math_operations/add_results.html.erb" })
+  end
+
+  def multiplier
+    render({ :template => "math_operations/multiply_form.html.erb" })
+  end
+
+  def multiply_results
+    @first = params.fetch("first_input").to_f
+    @second = params.fetch("second_input").to_f
+
+    @result = @second * @first
+    render({ :template => "math_operations/multiply_results.html.erb" })
+  end
+
+  def divider
+    render({ :template => "math_operations/divide_form.html.erb" })
+  end
+
+  def divide_results
+    @first = params.fetch("first_input").to_f
+    @second = params.fetch("second_input").to_f
+
+    @result = @first / @second
+    render({ :template => "math_operations/divide_results.html.erb" })
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,13 @@ Rails.application.routes.draw do
   get("/subtract", {:controller => "math", :action => "subtractor"})
   get("/wizard_subtract", {:controller => "math", :action => "subtractor_results"})
 
+  get("/add", {:controller => "math", :action => "adder"})
+  get("/wizard_add", {:controller => "math", :action => "adder_results"})
+
+  get("/multiply", {:controller => "math", :action => "multiplier"})
+  get("/wizard_multiply", {:controller => "math", :action => "multiply_results"})
+
+  get("/divide", {:controller => "math", :action => "divider"})
+  get("/wizard_divide", {:controller => "math", :action => "divide_results"})
+
 end
